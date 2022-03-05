8815728987ac685ee45530846e77cfdf9cab3449
Naveentn24 <naveensaven@gmail.com>
2022-03-05 11:16:09 +0530

    Final commit

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var projectObjects = [
         start_time: '01:10pm',
         end_time: '07:40pm',
         duartion: '6:30',
-        indicator:"info"
+        indicator: "info"
     },
     {
         id: 1,
@@ -15,7 +15,7 @@ var projectObjects = [
         start_time: '01:10pm',
         end_time: '07:40pm',
         duartion: '6:30',
-        indicator:"primary"
+        indicator: "primary"
 
     },
     {
@@ -25,7 +25,7 @@ var projectObjects = [
         start_time: '01:10pm',
         end_time: '07:40pm',
         duartion: '6:30',
-        indicator:"warning"
+        indicator: "warning"
 
     },
     {
@@ -35,15 +35,13 @@ var projectObjects = [
         start_time: '01:10pm',
         end_time: '07:40pm',
         duartion: '6:30',
-        indicator:"secondary"
+        indicator: "secondary"
     }
 ];
 
 function Update() {
-
     let projectList = document.querySelector('.project-list')
     let projectItems = ''
-
     for (project of projectObjects) {
         projectItems += `<tr>  
                             <td> <div class="me-3 badge  number number-${project.indicator}" >
@@ -54,20 +52,13 @@ function Update() {
                             <td> ${project.duartion} </td>
                         </tr>`
     }
-
     projectList.innerHTML = projectItems
-
 };
 Update();
 
-// Example starter JavaScript for disabling form submissions if there are invalid fields
 (function () {
     'use strict'
-
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
     var forms = document.querySelectorAll('.needs-validation')
-
-    // Loop over them and prevent submission
     Array.prototype.slice.call(forms)
         .forEach(function (form) {
             form.addEventListener('submit', function (event) {
@@ -75,45 +66,19 @@ Update();
                     event.preventDefault()
                     event.stopPropagation()
                 } else {
-                console.log('passed');
-                form.classList.add('was-validated')
-
-                // let nameInput = document.getElementById('validationCustom01');
-                // let dateInput = document.getElementById('validationCustom02');
-                // let startTimeInput = document.getElementById('validationCustom03');
-                // let endTimeInput = document.getElementById('validationCustom04');
-
-                // const name1 = nameInput.value;
-                // const date = dateInput.value;
-                // const start_time = startTimeInput.value;
-                // const end_time = endTimeInput.value;
-
-                // idInc = projectObjects[projectObjects.length - 1].id;
-
-                // projectObjects.push({
-                //     id: idInc + 1,
-                //     name: name1,
-                //     date: date,
-                //     start_time: start_time,
-                //     end_time: end_time,
-                //     duartion: '6:30'
-
-                // });
-            }
-            
-
-            //     console.log(projectObjects);
+                    console.log('passed');
+                    form.classList.add('was-validated')
+                }
             }, false)
-            
+
         })
-        // Update();
 })()
-function addRuler(){
-         jQuery("ul.nav.navbar-nav").append("<hr class='sidebar-sep'/>");
+function addRuler() {
+    jQuery("ul.nav.navbar-nav").append("<hr class='sidebar-sep'/>");
 }
 addRuler()
-(function(){
-    document.querySelector('#msbo').addEventListener('click', function(){
-      document.querySelector('body').classList.toggle('msb-x');
-      });
-  }());
+    (function () {
+        document.querySelector('#msbo').addEventListener('click', function () {
+            document.querySelector('body').classList.toggle('msb-x');
+        });
+    }());
