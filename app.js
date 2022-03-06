eb2881339231df3d2a9381c762d2f2350c507647
Naveentn24 <naveensaven@gmail.com>
2022-03-06 23:42:02 +0530

    Changed

--- app.js
+++ app.js
@@ -1,42 +1,42 @@
 var projectObjects = [
-    {
-        id: 0,
-        name: 'Deadlift',
-        date: 'May 21 2020',
-        start_time: '01:10pm',
-        end_time: '07:40pm',
-        duartion: '6:30',
-        indicator: "info"
-    },
-    {
-        id: 1,
-        name: 'Deadlift',
-        date: 'May 21 2020',
-        start_time: '01:10pm',
-        end_time: '07:40pm',
-        duartion: '6:30',
-        indicator: "primary"
+{
+    id: 0,
+    name: 'Deadlift',
+    date: 'May 21 2020',
+    start_time: '01:10pm',
+    end_time: '07:40pm',
+    duartion: '6:30',
+    indicator: "info"
+},
+{
+    id: 1,
+    name: 'Deadlift',
+    date: 'May 21 2020',
+    start_time: '01:10pm',
+    end_time: '07:40pm',
+    duartion: '6:30',
+    indicator: "primary"
 
-    },
-    {
-        id: 2,
-        name: 'Deadlift',
-        date: 'May 21 2020',
-        start_time: '01:10pm',
-        end_time: '07:40pm',
-        duartion: '6:30',
-        indicator: "warning"
+},
+{
+    id: 2,
+    name: 'Deadlift',
+    date: 'May 21 2020',
+    start_time: '01:10pm',
+    end_time: '07:40pm',
+    duartion: '6:30',
+    indicator: "warning"
 
-    },
-    {
-        id: 3,
-        name: 'Deadlift',
-        date: 'May 21 2020',
-        start_time: '01:10pm',
-        end_time: '07:40pm',
-        duartion: '6:30',
-        indicator: "secondary"
-    }
+},
+{
+    id: 3,
+    name: 'Deadlift',
+    date: 'May 21 2020',
+    start_time: '01:10pm',
+    end_time: '07:40pm',
+    duartion: '6:30',
+    indicator: "secondary"
+}
 ];
 
 function Update() {
@@ -44,13 +44,13 @@ function Update() {
     let projectItems = ''
     for (project of projectObjects) {
         projectItems += `<tr>  
-                            <td> <div class="me-3 badge  number number-${project.indicator}" >
-                            ${project.id}</div>${project.name}  </td>
-                            <td> ${project.date} </td>
-                            <td> ${project.start_time} </td>
-                            <td> ${project.end_time} </td>
-                            <td> ${project.duartion} </td>
-                        </tr>`
+        <td> <div class="me-3 badge  number number-${project.indicator}" >
+        ${project.id}</div>${project.name}  </td>
+        <td> ${project.date} </td>
+        <td> ${project.start_time} </td>
+        <td> ${project.end_time} </td>
+        <td> ${project.duartion} </td>
+        </tr>`
     }
     projectList.innerHTML = projectItems
 };
@@ -60,27 +60,26 @@ Update();
     'use strict'
     var forms = document.querySelectorAll('.needs-validation')
     Array.prototype.slice.call(forms)
-        .forEach(function (form) {
-            form.addEventListener('submit', function (event) {
-                if (!form.checkValidity()) {
-                    event.preventDefault()
-                    event.stopPropagation()
-                } else {
-                    console.log('passed');
-                    form.classList.add('was-validated')
-                }
-            }, false)
+    .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+            if (!form.checkValidity()) {
+                event.preventDefault()
+                event.stopPropagation()
+            } else {
+                console.log('passed');
+                form.classList.add('was-validated')
+            }
+        }, false)
 
-        })
+    })
 })();
 
 function addRuler() {
     jQuery("ul.nav.navbar-nav").append("<hr class='sidebar-sep'/>");
 }
 addRuler();
-
 (function () {
-    document.querySelector('#msbo').addEventListener('click', function () {
-        document.querySelector('body').classList.toggle('msb-x');
-    });
+ document.querySelector('#msbo').addEventListener('click', function () {
+    document.querySelector('body').classList.toggle('msb-x');
+});
 }());
